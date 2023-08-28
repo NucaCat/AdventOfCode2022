0b75a96ae49065a68604a75d51761f0a2d301f5f
Виктор Чумаченко <NucaCat@yandex,ru>
2023-08-28 09:12:37 +0300

    use top level statement

--- Main/Program.cs
+++ Main/Program.cs
@@ -1,14 +1,6 @@
 ﻿using System;
 using AdventOfCode;
 
-namespace Codewars;
-
-public class Program
-{
-    public static void Main()
-    {
-        // BenchmarkRunner.Run<AocBenchmarks>();
-        var result = AdventOfCode8.Part1();
-        Console.WriteLine(result);
-    }
-}
+// BenchmarkRunner.Run<AocBenchmarks>();
+var result = AdventOfCode8.Part1();
+Console.WriteLine(result);
